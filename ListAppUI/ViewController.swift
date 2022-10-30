0b315db0bc50ae31bff6db8b0c83005b3acf63cb
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2022-10-30 19:48:33 +0300

    Initial Commit

---
+++ ListAppUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListAppUI
+//
+//  Created by Vural Kayra Çetintaş on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
